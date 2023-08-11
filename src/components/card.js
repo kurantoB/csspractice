2becbdcc1bbfaac3fd6ac2e66118bde2529c5e2d
D L <laomeister93@gmail.com>
2023-08-10 22:49:03 -0400

    fix h1 issue, fix ellipses issue, fix image sizing

--- src/components/card.js
+++ src/components/card.js
@@ -4,7 +4,7 @@ export default function Card(props) {
     return (
         <div className='card'>
             <img className='card--img' src={props.imgsrc} />
-            <h1 className='card--header'>{props.header}</h1>
+            <h2 className='card--header'>{props.header}</h2>
             <span className='card--blurb'>{props.blurb}</span>
         </div>
     )
