a7c91429b273ee57165ec642635264cc2c78411b
D L <laomeister93@gmail.com>
2023-09-12 23:06:50 -0400

    font scaling

--- src/components/card.js
+++ src/components/card.js
@@ -5,7 +5,7 @@ export default function Card(props) {
         <div className='card'>
             <img className='card--img' src={props.imgsrc} />
             <div className='card--desc'>
-                <h2 className='card--header'>{props.header}</h2>
+                <h5 className='card--header'>{props.header}</h5>
                 <span className='card--blurb'>{props.blurb}</span>
             </div>
         </div>
